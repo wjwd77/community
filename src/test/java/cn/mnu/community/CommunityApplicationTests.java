c30eab6d713a662fa66467ec6f67d31961deec9a
nasozhang <nasozhang@qq.com>
2020-03-10 22:38:43 +0800

    init repo

---
+++ src/test/java/cn/mnu/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mnu.community;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
